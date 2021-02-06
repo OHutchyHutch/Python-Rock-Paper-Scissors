b076a1431f01ec8bbb47208cff600758dd9982f6
Owen <43101567+OHutchyHutch@users.noreply.github.com>
2021-02-05 22:48:47 -0500

    Update account.py

--- account.py
+++ account.py
@@ -5,13 +5,7 @@ while True:
   username = input("Pick a username:  ")
 
   print("Your account has been created!")
-  text_file = open("accounts.csv","a")
+  text_file = open("accounts.txt","a")
   text_file.write(username)
-  text_file.write(",")
-  #score
-  text_file.write("0")
-  text_file.write(",")
-  #draws
-  text_file.write("0")
-  text_file.write(",")
+  text_file.write("\n")
   text_file.close()
